328b3b5fda2196f68865b7db71bb28605ae62357
raulFuzita <raul_fuzita@hotmail.com>
2023-09-11 00:15:59 +0100

    Addtional app information

--- app/server.py
+++ app/server.py
@@ -8,6 +8,8 @@ app.before_request(default_limit_ip_requests)
 
 app.register_blueprint(http_request_bp, url_prefix='/api/v1/request')
 
+app.route('/version')(lambda: "app version: 1.0.0.0")
+
 if __name__ == '__main__':
     print("Server is running!")
     app.run(debug=True)
